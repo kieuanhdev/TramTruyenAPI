74275217087bfc34385a69c1f8dd2b9b4bedb096
kieuanhdev <kieuanh.dev@gmail.com>
2026-02-26 09:17:54 +0700

    create project

---
+++ src/test/java/com/tramtruyen/api/TramTruyenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tramtruyen.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramTruyenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
