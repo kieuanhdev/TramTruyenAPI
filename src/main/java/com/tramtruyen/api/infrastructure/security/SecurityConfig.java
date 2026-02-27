804ffa5f86619e35b64f3e68462d9c27c1075ff9
kieuanhdev <kieuanh.dev@gmail.com>
2026-02-27 12:03:05 +0700

    feat(auth): implement login api and jwt token generation with custom secret

--- src/main/java/com/tramtruyen/api/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/tramtruyen/api/infrastructure/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.tramtruyen.api.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -29,5 +31,10 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
     }
 }
